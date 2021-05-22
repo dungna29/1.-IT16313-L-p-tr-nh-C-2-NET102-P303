875baf28228f8f659c73b6d69a4fde8ae707c61e
Nguyễn Anh Dũng <dungna29@fpt.edu.vn>
2021-05-22 07:34:40 +0700

    1

--- 1_IT16313_NET102/BAI_1.3_DELEGATE_ACTION_FUNC/Program.cs
+++ 1_IT16313_NET102/BAI_1.3_DELEGATE_ACTION_FUNC/Program.cs
@@ -6,6 +6,29 @@ namespace BAI_1._3_DELEGATE_ACTION_FUNC
     class Program
     {
         #region  Bài Action, Predicate, Func: Delegate - Generic(Sử dụng sẵn tham số Generic để khai báo)
+        /*
+        Thay vì chúng ta phải khai báo định nghĩa trước delegate thì chúng ta sẽ sử dụng 3 kiểu dưới đây khai báo cho nhanh.
+         Action, Predicate, Func (Viết tắt là APF là cho nhanh) trong C#
+           + Action: Action<T in1, T in2, …>. Action tương đương 1 delegate với kiểu trả về là void, với in1, in2 là các params nhận vào.
+
+           + Predicate: Predicate<T in>. Predicate tương đương 1 delegate với kiểu trả về là bool, với in là các param nhận vào. Predicate chỉ có thể nhận vào 1 param duy nhất.
+
+           + Func: Func<T in1, T in2, … , T result>. Function tương đương 1 delegate với kiểu trả về do ta khai báo (result), in1, in2 là các params nhận vào. Func bắt buộc phải trả ra giá trị, không thể trả void.
+
+
+           Bảng so sánh cách khai báo bằng delegate cùng với cách khai báo tương ứng bằng Action, Predicate, Func:
+
+           delegate void VoidDelegate(int input1, bool input2)	- Action<int, bool>	
+
+           delegate bool BoolDelegate(int input1)	-  Predicate<int>	|| Func<int, bool>
+
+           delegate int intDelegate(bool input2)	- Func<bool, int>
+
+           delegate void HelloWorldDelegate()	- Action
+
+           delegate bool HelloWorldBoolDelegate()	- Predicate -  Func<bool>
+
+       */
         static void Info1(string s)
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
